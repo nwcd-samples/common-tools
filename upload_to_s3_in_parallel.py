7c9be110445ad5abc05e80c21a43b92a9f088c42
aqiao <vke@live.com>
2023-07-11 11:18:05 +0800

    duplicatly loop files in case the local_dir has no enough files

--- upload_to_s3_in_parallel.py
+++ upload_to_s3_in_parallel.py
@@ -14,18 +14,19 @@ from concurrent.futures import ThreadPoolExecutor, Future
 
 s3_client = boto3.client('s3')
 # 总共需要上传的文件个数
-total = 10
+total = 50
 # 已经上传的文件个数
 current = 0
 # s3 桶名字 不需要带s://
 bucket_name = "cunxin-bucket"
 # 本地文件路径
+# 请至少保证该目录下包含1个文件，工作线程会循环读取该目录下文件直到total
 local_dir = "/Users/aqiao/Work/WorkDocs/"
 # 最大并行度
-max_workers = 5
+max_workers = 10
 
 mutex = threading.Lock()
-# 创建线程池，最多维护10个线程。
+# 创建线程池，初始化max_workers个线程。
 pool = ThreadPoolExecutor(max_workers)
 
 
@@ -39,7 +40,6 @@ def upload_task(full_name, file_name):
                 current += 1
                 threading_name = threading.current_thread().name
                 print(f"thread {threading_name} is uploading {s3_file}")
-                time.sleep(1)
                 return s3_file
             else:
                 return ""
@@ -49,11 +49,15 @@ def upload_task(full_name, file_name):
 
 def upload_file():
     if os.path.exists(local_dir):
-        for item in os.listdir(local_dir):
-            full_name = os.path.join(local_dir, item)
-            if os.path.isfile(full_name):
-                future = pool.submit(upload_task, full_name, item)
-                future.add_done_callback(done)
+        # In case the quantity of files under local_dir is less than total
+        # use the While loop to continue
+        while current < total:
+            for item in os.listdir(local_dir):
+                full_name = os.path.join(local_dir, item)
+                if os.path.isfile(full_name):
+                    future = pool.submit(upload_task, full_name, item)
+                    future.add_done_callback(done)
+            time.sleep(1)
     else:
         print(f"{local_dir} is not existed")
 
@@ -65,8 +69,4 @@ def done(response):
 
 if __name__ == '__main__':
     upload_file()
-    while True:
-        if current >= total:
-            pool.shutdown(True)
-            break;
-        time.sleep(1)
+    pool.shutdown(True)
